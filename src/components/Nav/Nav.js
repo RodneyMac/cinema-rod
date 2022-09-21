fe004fc4ee563b91e18c773e99d63c4817fb2b00
RodneyMac <rodneydavidmaciel@gmail.com>
2022-09-21 15:28:54 -0300

    Nav component added, Tailwind framework installed

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div className='h-10 bg-gray-600 text-white flex justify-center items-center'>Nav</div>
+  )
+}
+
+export default Nav;
